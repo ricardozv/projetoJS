50cb0d60e4f62f2432354f4ed403f5e8c9cea3e8
ricardozv <ricardosilvanet03@gmail.com>
2020-05-12 00:58:29 -0300

    up DevController.js

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -3,8 +3,31 @@ const axios = require('axios');
 const Dev = require('../models/Dev');
 
 module.exports = { 
+    async index(req, res) {
+        const { user } = req.headers;
+
+        const loggedDev = await Dev.findById(user);
+
+        const users = await Dev.find({
+            $and:[
+                {_id: { $ne: user } },
+                {_id: { $nin: loggedDev.likes } },
+                {_id: { $nin: loggedDev.dislikes } }
+            ],
+        })
+
+        return res.json(users);
+    },
+
+
     async store(req, res) {
         const { username } = req.body;
+
+        const userExists = await Dev.findOne({ user: username });
+
+        if (userExists) {
+            return res.json(userExists);
+        }
 
         const response = await axios.get(`https://api.github.com/users/${username}`);
 
