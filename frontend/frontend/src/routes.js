297187268e9d7bdd285e70e4241c27def6a07db4
ricardozv <ricardosilvanet03@gmail.com>
2020-05-19 22:34:03 -0300

    uploads routes.js

--- frontend/frontend/src/routes.js
+++ frontend/frontend/src/routes.js
@@ -9,7 +9,7 @@ export default function Routes() {
     return (
         <BrowserRouter>
         <Route path = "/" exact component={Login} />
-        <Route path="/main" component={Main} />
+        <Route path="/dev/:id" component={Main} />
         
         </BrowserRouter>
     );
